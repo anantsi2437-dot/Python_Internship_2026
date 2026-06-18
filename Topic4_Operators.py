56a6dfc7eef77b70286f2a38cbf1f5c467cba961
anantsi2437-dot <anantsi2437@gmail.com>
2026-06-18 20:53:22 +0530

    Add files via upload

---
+++ Topic4_Operators.py
@@ -0,0 +1,28 @@
+# 1. Arithmetic operations
+a = 20
+b = 10
+
+print("Addition =", a + b)
+print("Subtraction =", a - b)
+print("Multiplication =", a * b)
+print("Division =", a / b)
+
+# 2. Average
+x = 10
+y = 20
+z = 30
+
+avg = (x + y + z) / 3
+print("Average =", avg)
+
+# 3. Modulus
+print("Remainder =", a % b)
+
+# 4. Square
+num = 5
+print("Square =", num ** 2)
+
+# 5. Area of Circle
+radius = 7
+area = 3.14 * radius * radius
+print("Area =", area)
