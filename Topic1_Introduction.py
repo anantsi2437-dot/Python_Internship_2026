0884ddb46e14344f98b4d7333d2b080c81b20942
anantsi2437-dot <anantsi2437@gmail.com>
2026-06-18 20:51:37 +0530

    Add files via upload

---
+++ Topic1_Introduction.py
@@ -0,0 +1,13 @@
+print("Hello World")
+
+print("Name: Anant Singh")
+print("Course: BTech AI")
+
+from datetime import date
+print("Today's Date:", date.today())
+
+print("Welcome to Python Development Internship")
+
+print("Python")
+print("Development")
+print("Internship")
