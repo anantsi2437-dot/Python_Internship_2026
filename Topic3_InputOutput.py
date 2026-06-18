80628e6e5edd2bcae2ab611a446db4a6d63f12b5
anantsi2437-dot <anantsi2437@gmail.com>
2026-06-18 20:52:46 +0530

    Add files via upload

---
+++ Topic3_InputOutput.py
@@ -0,0 +1,24 @@
+# 1. Name input and greeting
+name = input("Enter your name: ")
+print("Hello,", name)
+
+# 2. Sum of two numbers
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+print("Sum =", a + b)
+
+# 3. Age input
+age = int(input("Enter your age: "))
+print("Age =", age)
+
+# 4. City input
+city = input("Enter your city: ")
+print("City =", city)
+
+# 5. Student details
+name = input("Enter student name: ")
+roll = input("Enter roll number: ")
+
+print("Student Details")
+print("Name:", name)
+print("Roll No:", roll)
