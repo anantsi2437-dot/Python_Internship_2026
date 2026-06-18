8bce7ffb8cc2718c4c6364f30323ff7c632387d2
anantsi2437-dot <anantsi2437@gmail.com>
2026-06-18 20:52:15 +0530

    Add files via upload

---
+++ Topic2_Variables.py
@@ -0,0 +1,28 @@
+name = "Anant Singh"
+age = 20
+marks = 85
+
+print(name)
+print(age)
+print(marks)
+
+a = 10
+b = 10.5
+c = "Python"
+d = True
+
+print(a, b, c, d)
+
+length = 10
+width = 5
+area = length * width
+print("Area =", area)
+
+emp_name = "Rahul"
+salary = 30000
+print(emp_name, salary)
+
+x = 5
+y = 10
+x, y = y, x
+print(x, y)
