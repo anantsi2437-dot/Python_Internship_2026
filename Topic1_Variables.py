05bc0020c34e390117b6e2c6f801c00bc30ab93f
anantsi2437-dot <anantsi2437@gmail.com>
2026-06-19 21:27:43 +0530

    Add files via upload

---
+++ Topic1_Variables.py
@@ -0,0 +1,38 @@
+# Student Details
+name = "Anant Singh"
+age = 20
+course = "BTech AI"
+
+print("Name:", name)
+print("Age:", age)
+print("Course:", course)
+
+# Different Data Types
+a = 10
+b = 10.5
+c = "Python"
+d = True
+
+print(a)
+print(b)
+print(c)
+print(d)
+
+# Area of Rectangle
+length = 10
+width = 5
+area = length * width
+print("Area =", area)
+
+# Perimeter of Rectangle
+perimeter = 2 * (length + width)
+print("Perimeter =", perimeter)
+
+# Swap Variables
+x = 5
+y = 10
+
+x, y = y, x
+
+print("x =", x)
+print("y =", y)
